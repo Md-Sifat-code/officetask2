5846c3def4963116a6c8c943815c04f32270f348
AnisulHaqueNiloy <niloy2931@gmail.com>
2025-05-21 03:22:51 +0600

    f

--- src/components/Investment/WhyShould.tsx
+++ src/components/Investment/WhyShould.tsx
@@ -1,7 +1,10 @@
+import card from '/assets/card.jpg'
+
 const WhyShould: React.FC = () => {
     interface InfoItem {
         title: string;
         description: string[];
+        img: string
     }
     let data: InfoItem[] = [
         {
@@ -11,7 +14,23 @@ const WhyShould: React.FC = () => {
                 "Relatively competitive property prices compared to other major global cities",
                 "Strong potential for long-term ROI",
             ],
+            img: card
         },
+
+        {
+            title: "Lifestyle Advantages",
+            description: [
+                "Year-round warm weather",
+                "Tax-free income environment",
+                "Low crime rate",
+                "Access to high-quality education and healthcare systems",
+
+            ],
+            img: card
+        },
+
+
+
 
 
         {
@@ -21,10 +40,8 @@ const WhyShould: React.FC = () => {
                 "A key global aviation link with excellent international connectivity",
 
             ],
+            img: card
         },
-
-
-
 
         {
             title: "Empowering the Next Generation",
@@ -33,14 +50,14 @@ const WhyShould: React.FC = () => {
                 "Give children the tools to shape their future.",
                 "Hope grows where knowledge is planted.",
             ],
+            img: card
         },
         {
-            title: "Environmental Awareness",
+            title: "Modern Infrastructure & Living Standardsv",
             description: [
-                "Our planet needs conscious choices.",
-                "Small actions lead to big impact.",
-                "Protect nature — it protects us.",
+                "Dubai’s modern infrastructure and high standard of living continue to make it a sought-after destination for property investments.",
             ],
+            img: card
         },
         {
             title: "Community and Unity",
@@ -49,6 +66,7 @@ const WhyShould: React.FC = () => {
                 "Support your neighbors, lift each other up.",
                 "Unity is the heartbeat of hope.",
             ],
+            img: card
         }
     ];
 
@@ -59,8 +77,13 @@ const WhyShould: React.FC = () => {
 
 
             <div className='max-w-7xl mx-auto py-14 space-y-8'>
-                <h1 className='text-5xl text-[#F4A896] text-center'>Why Should You Make a Real Estate Investment in Dubai?</h1>
-                <p className='text-[#358597] text-center'>Investing in property in Dubai offers significant potential for those who approach the process with thorough research, careful planning, and professional guidance. The city’s real estate market has become increasingly attractive due to its strong potential for long-term returns on investment (ROI) and various appealing factors for both local and international buyers.</p>
+                <div>
+                    <h1 className='text-5xl text-[#F4A896] text-center'>Why Should You Make a Real Estate Investment in Dubai?</h1>
+                    <p className='text-[#358597] text-center'>Investing in property in Dubai offers significant potential for those who approach the process with thorough research, careful planning, and professional guidance. The city’s real estate market has become increasingly attractive due to its strong potential for long-term returns on investment (ROI) and various appealing factors for both local and international buyers.</p>
+                </div>
+                <div>{data.map((item, idx) => (
+                    <div>{item.title}</div>
+                ))}</div>
             </div>
         </div>
     );

32e19bd39c401643cb412d84414cb60a4fcbd988
Md-Sifat-code <codewithsifat4@gmail.com>
2025-05-23 00:35:39 +0600

    base url updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath : 'baseApi',
-    baseQuery : fetchBaseQuery({baseUrl : "https://disaster-relief-donation.vercel.app/api/v1"}),
+    baseQuery : fetchBaseQuery({baseUrl : "https://eddmwd-server.onrender.com"}),
     tagTypes:['donations','testimonials','comments','volunteers'],
     endpoints : () => ({
      

2960f0c641581bbcf4700c0395c184e44a784b04
Sohag00879 <shishirsohagislam@gmail.com>
2025-05-22 22:30:43 +0600

    rent details page responsive done

--- src/rentDetails/RentHero.tsx
+++ src/rentDetails/RentHero.tsx
@@ -1,28 +1,28 @@
+import logo from '/assets/logo.png';
 
-import logo from '/assets/logo.png'
 const RentHero = () => {
   return (
-     <div>
-      <div className="hero-wrapper h-[650px] flex">
-        {/* Left Side with Background Image */}
-        <div className="left-wrapper bg-[url('/assets/rent-details-hero-1.png')] bg-cover bg-center w-[60%] h-full me-2">
-          <div className="flex justify-center items-center h-full">
-            <img src={logo} alt="logo" />
-          </div>
+    <div className="w-full">
+      <div className="hero-wrapper flex flex-col md:flex-row h-auto md:h-[650px] gap-2">
+        
+        {/* Left Side */}
+        <div className="left-wrapper bg-[url('/assets/rent-details-hero-1.png')] bg-cover bg-center w-full md:w-[60%] h-[300px] md:h-full flex justify-center items-center">
+          <img src={logo} alt="logo" className="w-32 md:w-40 lg:w-48" />
         </div>
 
-        {/* Right Side with Two Stacked Background Images */}
-        <div className="right-wrapper w-[40%] h-full flex flex-col">
-          <div className="h-1/2 bg-[url('/assets/rent-details-hero-2.png')] bg-cover bg-center flex justify-center items-center mb-2">
-            <img src={logo} alt="logo" />
+        {/* Right Side */}
+        <div className="right-wrapper w-full md:w-[40%] flex flex-col gap-2">
+          <div className="h-[150px] md:h-1/2 bg-[url('/assets/rent-details-hero-2.png')] bg-cover bg-center flex justify-center items-center">
+            <img src={logo} alt="logo" className="w-24 md:w-32 lg:w-40" />
           </div>
-          <div className="h-1/2 bg-[url('/assets/rent-details-hero-2.png')] bg-cover bg-center flex justify-center items-center">
-            <img src={logo} alt="logo" />
+          <div className="h-[150px] md:h-1/2 bg-[url('/assets/rent-details-hero-2.png')] bg-cover bg-center flex justify-center items-center">
+            <img src={logo} alt="logo" className="w-24 md:w-32 lg:w-40" />
           </div>
         </div>
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default RentHero;
